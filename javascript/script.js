f1716d1b006dcc36d2c2e2540d71fb3a35bdfae8
madhu <thatiparthymadhumathi@gmail.com>
2026-02-27 16:07:41 +0530

    completed

---
+++ javascript/script.js
@@ -0,0 +1,67 @@
+/*console.log("Hello world !");
+//Reinitlaize and Redueclare
+var a;
+a=10;
+var a;
+a=20;
+//reinitialize
+let b=10;
+//let b;
+const c = 30;
+console.log(a);
+console.log(b);
+console.log(c);*/
+
+var num=10;//number
+var str="sushuu";//'' "" ``//string
+var bool=true;//boolean
+var un;//undefined
+var nu=null;//object
+var big=1234567890987654n; //bigint
+var symbol=Symbol('li');//symbol
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof un);
+console.log(typeof nu);
+console.log(typeof big);
+console.log(typeof symbol);
+
+var arr=[1,2,3,4,5] //array
+console.log(typeof arr); //object
+
+var Obj ={
+    name:"Vikram",
+    dept:"DS"
+}
+console.log(Obj);
+console.log(typeof Obj);
+
+var a=10;
+var b="20";
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b);
+
+var a=10;
+var b="20";
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a<b);
+console.log(a>b);
+console.log(a<=b);
+console.log(a>=b);
+
+
+var a = true;
+var b = false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!b);
+
